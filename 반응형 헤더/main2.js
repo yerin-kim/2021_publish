78e5e7e7724a82ec6cb80c4e8e6b5adc398c54cc
yerin-kim <54825164+yerin-kim@users.noreply.github.com>
2021-02-12 02:17:23 +0900

    2

---
+++ 반응형 헤더/main2.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.querySelector('.nav_toggleBtn');
+const menu = document.querySelector('.nav_menu');
+const icons = document.querySelector('.nav_icons');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  icons.classList.toggle('active');
+});
+
